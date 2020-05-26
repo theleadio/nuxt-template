befbcaa9afc8d6ea024ffe1b0c8273fb4f0b4564
Dr. Lau Cher Han <laucherhan@gmail.com>
2020-05-27 06:22:04 +0800

    implemented basic css

--- pages/checkin.vue
+++ pages/checkin.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    <form class="w-full max-w-sm">
-      <div>
-        <input type="text" placeholder="Enter your name" aria-label="Full name">
+    <div class='mt-64 text-center'>
+      <h1 class='text-3xl'>TickTick</h1>
+      <h2 class='text-xl'>Contactless attendance solution</h2>
+    </div>
+    <form class="w-full">
+      <div class='flex items-center border-b border-b-2 mx-auto w-64 mt-5'>
+        <input class='focus:outline-none' type="text" placeholder="Enter your name" aria-label="Full name">
       </div>
       
-      <div>
-        <input type="tel" placeholder="Enter your mobile" aria-label="Full name">
+      <div class='text-center flex items-center border-b border-b-2 mx-auto w-64 mt-5'>
+        <input class='focus:outline-none' type="tel" placeholder="Enter your mobile" aria-label="Full name">
       </div>
 
-      <button>Check in</button>
+      <div class="text-center mt-5">
+        <button class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="button">
+      Check in
+    </button>
+      </div>
+
 
     </form>
   </div>
